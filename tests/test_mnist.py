b3c86c0f18396d7a35e5279de987188ba1a49f10
Kevin Buhler <buhlerkw@gmail.com>
2023-07-23 18:00:56 -0700

    optimizer params just like torch

--- tests/test_mnist.py
+++ tests/test_mnist.py
@@ -21,28 +21,28 @@ class SimpleMLP:
 
   def forward(self, x):
     return x.dot(self.l1).relu().dot(self.l2).logsoftmax()
+  
+  def parameters(self):
+    return [self.l1, self.l2]
 
 class SimpleConvNet:
   def __init__(self):
     # from https://keras.io/examples/vision/mnist_convnet/
     conv = 3
     inter_chan, out_chan = 8, 16 # for speed
-    self.c1 = Tensor(dense_layer(inter_chan,1,conv,conv))
-    self.c2 = Tensor(dense_layer(out_chan,inter_chan,conv,conv))
-    self.l1 = Tensor(dense_layer(out_chan*5*5, 10))
-    # self.c1 = Tensor(dense_layer(channels,1,conv_size,conv_size))     # (num_filters, color_channels, kernel_h, kernel_w)
-    # self.l1 = Tensor(dense_layer((28-conv_size+1)**2*channels, 128))  # (28-conv+1)(28-conv+1) since kernel isn't padded
-    # self.l2 = Tensor(dense_layer(128, 10))                            # MNIST output is 10 classes
+    self.c1 = Tensor(dense_layer(inter_chan,1,conv,conv))               # (num_filters, color_channels, kernel_h, kernel_w)
+    self.c2 = Tensor(dense_layer(out_chan,inter_chan,conv,conv))        # (28-conv+1)(28-conv+1) since kernel isn't padded
+    self.l1 = Tensor(dense_layer(out_chan*5*5, 10))                     # MNIST output is 10 classes
 
   def forward(self, x):
     x.data = x.data.reshape((-1, 1, 28, 28))                            # get however many number of imgs in batch
-    x = x.conv2d(self.c1).relu().max_pool2d()
-    x = x.conv2d(self.c2).relu().max_pool2d()
-    x = x.reshape(Tensor(np.array((x.shape[0], -1))))
-    return x.dot(self.l1).logsoftmax()
-    # x = x.conv2d(self.c1).relu()                                      # pass through conv first
-    # x = x.reshape(Tensor(np.array((x.shape[0], -1))))
-    # return x.dot(self.l1).relu().dot(self.l2).logsoftmax()            # then go down to mlp and softmax to get probs
+    x = x.conv2d(self.c1).relu().max_pool2d()                           # pass through layer 1 first
+    x = x.conv2d(self.c2).relu().max_pool2d()                           # pass through layer 2
+    x = x.reshape(Tensor(np.array((x.shape[0], -1))))                   # then go down to mlp
+    return x.dot(self.l1).logsoftmax()                                  # softmax to get probs   
+
+  def parameters(self):
+    return [self.l1, self.c1, self.c2]  
 
 def train(model, optimizer, steps, BS=128):
   # ********* training the model *********
@@ -55,7 +55,6 @@ def train(model, optimizer, steps, BS=128):
 
     # X_train[samp] is selecting a random batch of training examples
     # 28x28 pixel size of MNIST images
-    # TODO: why reshaping?
     x = Tensor(X_train[samp].reshape((-1, 28*28)).astype(np.float32))
     Y = Y_train[samp]
 
@@ -73,8 +72,7 @@ def train(model, optimizer, steps, BS=128):
     model_outputs = model.forward(x)
 
     # ********* backward pass *********
-    # NLL loss
-    loss = model_outputs.mul(y).mean() # TODO: what is NLL loss function?
+    loss = model_outputs.mul(y).mean() # TODO: what exactly is NLL loss function?
     loss.backward()
     optimizer.step()
 
@@ -100,20 +98,19 @@ class TestMNIST(unittest.TestCase):
   def test_mnist_conv_adam(self):
     np.random.seed(1337)
     model = SimpleConvNet()
-    optimizer = optim.Adam([model.c1, model.c2, model.l1], lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
     train(model, optimizer, steps=400)
     evaluate(model)
   def test_mnist_mlp_sgd(self):
     np.random.seed(369)
     model = SimpleMLP()
-    optimizer = optim.SGD([model.l1, model.l2], lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
     train(model, optimizer, steps=1000)
     evaluate(model)
   def test_mnist_mlp_rmsprop(self):
     np.random.seed(369)
     model = SimpleMLP()
-    np.random.seed(369)
-    optimizer = optim.RMSprop([model.l1, model.l2], lr=0.0002)
+    optimizer = optim.RMSprop(model.parameters(), lr=0.0002)
     train(model, optimizer, steps=1000)
     evaluate(model)
 
