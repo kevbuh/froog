21a24501ba1e767f1ecbddcaf59bd13aab040e83
Kevin Buhler <buhlerkw@gmail.com>
2025-05-05 10:35:59 -0600

    repair efficientnet

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -18,43 +18,16 @@ Environment Variables:
   VIZ=1 --> plots processed image and output probabilities
 
 How to Run:
-  'VIZ=1 python models/efficientnet.py https://your_image_url'
+  'VIZ=1 python3 models/efficientnet.py <https://optional_image_url>'
 
-EfficientNet Hyper-Parameters and Weights:
-url_map = {
-    'efficientnet-b0': 'https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/efficientnet-b0-355c32eb.pth',
-    'efficientnet-b1': 'https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/efficientnet-b1-f1951068.pth',
-    'efficientnet-b2': 'https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/efficientnet-b2-8bb594d6.pth',
-    'efficientnet-b3': 'https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/efficientnet-b3-5fb5a3c3.pth',
-    'efficientnet-b4': 'https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/efficientnet-b4-6ed6700e.pth',
-    'efficientnet-b5': 'https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/efficientnet-b5-b6417697.pth',
-    'efficientnet-b6': 'https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/efficientnet-b6-c76e70fd.pth',
-    'efficientnet-b7': 'https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/efficientnet-b7-dcc49843.pth',
-}
+Output: 
+  ******************************
+  inference 9.63 s
 
-params_dict = {
-        # Coefficients:   width,depth,res,dropout
-        'efficientnet-b0': (1.0, 1.0, 224, 0.2),
-        'efficientnet-b1': (1.0, 1.1, 240, 0.2),
-        'efficientnet-b2': (1.1, 1.2, 260, 0.3),
-        'efficientnet-b3': (1.2, 1.4, 300, 0.3),
-        'efficientnet-b4': (1.4, 1.8, 380, 0.4),
-        'efficientnet-b5': (1.6, 2.2, 456, 0.4),
-        'efficientnet-b6': (1.8, 2.6, 528, 0.5),
-        'efficientnet-b7': (2.0, 3.1, 600, 0.5),
-        'efficientnet-b8': (2.2, 3.6, 672, 0.5),
-        'efficientnet-l2': (4.3, 5.3, 800, 0.5),
-    }
-
-blocks_args = [
-        'r1_k3_s11_e1_i32_o16_se0.25',
-        'r2_k3_s22_e6_i16_o24_se0.25',
-        'r2_k5_s22_e6_i24_o40_se0.25',
-        'r3_k3_s22_e6_i40_o80_se0.25',
-        'r3_k5_s11_e6_i80_o112_se0.25',
-        'r4_k5_s22_e6_i112_o192_se0.25',
-        'r1_k3_s11_e6_i192_o320_se0.25',
-    ]
+  imagenet class: 254
+  prediction    : pug, pug-dog
+  probability   : 0.96068513
+  ******************************
 """
 
 import io
@@ -175,7 +148,8 @@ class EfficientNet:
   def load_weights_from_torch(self):
     import torch
     b0 = fetch("https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/efficientnet-b0-355c32eb.pth")
-    b0 = torch.load(io.BytesIO(b0))
+    with open(b0, 'rb') as f:
+      b0 = torch.load(io.BytesIO(f.read()))
 
     for k,v in b0.items():
       if '_blocks.' in k:
@@ -194,7 +168,8 @@ class EfficientNet:
         mv.gpu_()
 
 def processImage(url):
-  img = Image.open(io.BytesIO(fetch(url)))
+  with open(fetch(url), 'rb') as f:
+    img = Image.open(io.BytesIO(f.read()))
   aspect_ratio = img.size[0] / img.size[1]
   img = img.resize((int(224*max(aspect_ratio,1.0)), int(224*max(1.0/aspect_ratio,1.0))))     
   img = np.array(img)
