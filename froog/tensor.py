01b2799e6b62458d24ef9b51e9ad8d8b65aadf6e
Kevin Buhler <buhlerkw@gmail.com>
2025-05-08 00:56:43 -0600

    big tensor update. typing, comments, more properties

--- froog/tensor.py
+++ froog/tensor.py
@@ -9,7 +9,11 @@
 import os
 import numpy as np
 from inspect import signature
+from typing import Tuple, List, Union, Optional, Any, TypeVar, cast
 from froog.gpu.gpu_utils import GPU, tensor_to_cpu, tensor_to_gpu, cl_ctx, cl_queue, init_gpu, is_buffer, cl
+
+# Type variable for Tensor
+T = TypeVar('T', bound='Tensor')  # For self-referential types
 
 # ************ Main Classes ************
 # ********** Tensor, Function **********
@@ -21,7 +25,7 @@ from froog.gpu.gpu_utils import GPU, tensor_to_cpu, tensor_to_gpu, cl_ctx, cl_qu
 
 class Tensor:
   did_float_warning = False
-  def __init__(self, data, gpu=False):
+  def __init__(self, data: Union[List, np.ndarray, Any], gpu: bool = False):
     if isinstance(data, list):
       data = np.array(data, dtype=np.float32)
     elif is_buffer(data):
@@ -39,50 +43,172 @@ class Tensor:
 
     # internal variables used for autograd graph construction
     self.data = data
-    self.grad = None
+    self.grad: Optional[Tensor] = None
     self._ctx = None # these are where the backward gradient computation are saved
     if gpu: self.gpu_()
 
-  def __repr__(self):
+  def __repr__(self) -> str:
     return f"Tensor data: {self.data}, gradients: {self.grad.data if self.grad else None}" 
   
-  def assign(self, x):
+  def assign(self, x: T) -> None:
     self.data = x.data
 
   # ********** Properties **********
 
   @property
-  def shape(self):
+  def shape(self) -> Tuple[int, ...]:
+    """
+    The shape of the tensor as a tuple of dimensions.
+    """
     return self.data.shape
+  
+  @property
+  def size(self) -> int:
+    """
+    Total number of elements in the tensor.
+    """
+    return int(np.prod(self.shape))
+  
+  @property
+  def ndim(self) -> int:
+    """
+    Number of dimensions (rank) of the tensor.
+    """
+    return len(self.shape)
+  
+  @property
+  def transpose(self) -> T:
+    """
+    Transpose of the tensor.
+    """
+    if isinstance(self.data, np.ndarray):
+      return Tensor(self.data.T, gpu=self.gpu)
+    else:
+      # For GPU tensor, we need to bring it back to CPU, transpose, then back to GPU
+      cpu_tensor = self.to_cpu()
+      return Tensor(cpu_tensor.data.T, gpu=self.gpu)
+  
+  @property
+  def dtype(self) -> np.dtype:
+    """
+    Data type of the tensor.
+    """
+    return self.data.dtype
+  
+  @property
+  def is_gpu(self) -> bool:
+    """
+    True if the tensor is on GPU.
+    """
+    return self.gpu
   
   # ********** Methods **********
   
   @staticmethod
-  def zeros(*shape):
+  def zeros(*shape: int) -> T:
+    """
+    Creates a tensor filled with zeros of the specified shape.
+    """
     return Tensor(np.zeros(shape, dtype=np.float32))
   
   @staticmethod
-  def ones(*shape):
+  def ones(*shape: int) -> T:
+    """
+    Creates a tensor filled with ones of the specified shape.
+    """
     return Tensor(np.ones(shape, dtype=np.float32))
   
   @staticmethod
-  def randn(*shape):
+  def randn(*shape: int) -> T:
+    """
+    Creates a tensor with random values from a normal distribution.
+    """
     return Tensor(np.random.randn(*shape).astype(np.float32))
   
   @staticmethod
-  def eye(dim):
+  def eye(dim: int) -> T:
+    """
+    Creates a 2D identity tensor of shape (dim, dim).
+    """
     return Tensor(np.eye(dim).astype(np.float32))
 
   @staticmethod
-  def arange(start, stop=None, step=1):
+  def arange(start: Union[int, float], stop: Optional[Union[int, float]] = None, step: Union[int, float] = 1) -> T:
+    """
+    Creates a 1D tensor with evenly spaced values.
+    
+    If stop is None, start is interpreted as 0 and start becomes the stop value.
+    """
     if stop is None:
       stop = start
       start = 0
     return Tensor(np.arange(start, stop, step, dtype=np.float32))
+  
+  def flatten(self) -> T:
+    """
+    Returns a flattened (1D) copy of the tensor.
+    """
+    return Tensor(self.data.reshape(-1), gpu=self.gpu)
+  
+  def detach(self) -> T:
+    """
+    Returns a new tensor detached from the current computation graph.
+    """
+    return Tensor(self.data.copy(), gpu=self.gpu)
+  
+  def view(self, *shape: int) -> T:
+    """
+    Returns a tensor with the same data but different shape.
+    """
+    return Tensor(self.data.reshape(shape), gpu=self.gpu)
+  
+  def unsqueeze(self, dim: int) -> T:
+    """
+    Adds a dimension of size 1 at the specified position.
+    """
+    shape = list(self.shape)
+    if dim < 0:
+        dim = len(shape) + 1 + dim
+    shape.insert(dim, 1)
+    return Tensor(self.data.reshape(shape), gpu=self.gpu)
+  
+  def squeeze(self, dim: Optional[int] = None) -> T:
+    """
+    Removes dimensions of size 1.
+    
+    If dim is specified, only removes the dimension at that position if it's 1.
+    """
+    if dim is None:
+      return Tensor(self.data.squeeze(), gpu=self.gpu)
+    else:
+      shape = list(self.shape)
+      if dim < 0:
+        dim = len(shape) + dim
+      if 0 <= dim < len(shape) and shape[dim] == 1:
+        shape.pop(dim)
+      return Tensor(self.data.reshape(shape), gpu=self.gpu)
+  
+  def to_float(self) -> T:
+    """
+    Converts tensor to float32 data type.
+    """
+    return Tensor(self.data.astype(np.float32), gpu=self.gpu)
+  
+  def to_int(self) -> T:
+    """
+    Converts tensor to int32 data type.
+    """
+    return Tensor(self.data.astype(np.int32), gpu=self.gpu)
+  
+  def to_bool(self) -> T:
+    """
+    Converts tensor to boolean data type.
+    """
+    return Tensor(self.data.astype(bool), gpu=self.gpu)
 
   # ********** Backward **********
 
-  def backward(self, allow_fill=True): 
+  def backward(self, allow_fill: bool = True) -> None: 
     if self._ctx is None:
       return
 
@@ -93,7 +219,7 @@ class Tensor:
 
     assert self.grad is not None
     
-    # THIS IS WHERE AUTO GRAD IS DONE
+    # NOTE: THIS IS WHERE AUTO GRAD IS DONE
     grads = self._ctx.backward(self._ctx, self.grad.data) # get gradients respective to what op happened
     if len(self._ctx.parents) == 1:
       grads = [grads]
@@ -108,7 +234,7 @@ class Tensor:
 
   # ****** cpu/gpu ******
     
-  def to_cpu(self):
+  def to_cpu(self) -> T:
     if self.gpu:
       data = tensor_to_cpu(self)
       ret = Tensor(data)
@@ -116,13 +242,13 @@ class Tensor:
         ret.grad = self.grad.to_cpu()
       return ret
     else: 
-      return self
+      return cast(T, self)
     
-  def gpu_(self):
+  def gpu_(self) -> None:
     self.data = self.to_gpu().data
     self.gpu = True
   
-  def to_gpu(self):
+  def to_gpu(self) -> T:
     if not GPU:
       raise Exception("no gpu support! install pyopencl")
     if not self.gpu:
@@ -132,22 +258,22 @@ class Tensor:
         ret.grad = self.grad.to_gpu()
       return ret
     else:
-      return self 
+      return cast(T, self) 
 
   ops = {}     # stores operations that are done on the CPU
   ops_gpu = {} # stores operations that are done on the GPU
 
   # ****** basic tensor math ops ******
 
-  def mean(self):
+  def mean(self) -> T:
     div = Tensor(np.array([1 / np.prod(self.shape)], dtype=self.data.dtype), gpu=self.gpu)
     return self.sum().mul(div)
   
-  def sqrt(self):
+  def sqrt(self) -> T:
     root = Tensor(np.zeros(self.shape, dtype=self.data.dtype)+0.5, gpu=self.gpu)
     return self.pow(root)
 
-  def div(self, y):
+  def div(self, y: T) -> T:
     root = Tensor(np.zeros(self.shape, dtype=self.data.dtype)-1, gpu=self.gpu)
     return self.mul(y.pow(root))
 
@@ -159,19 +285,23 @@ class Tensor:
                     
 class Function:
   """
-  An instantiation of the Function class includes the context
+  Base class for all operations. Stores context for autograd graph.
   """
-  def __init__(self, *tensors):
+  def __init__(self, *tensors: Tensor) -> None:
     self.parents = tensors
-    self.saved_tensors = []
+    self.saved_tensors: List[Any] = []
 
-  def save_for_backward(self, *x):
+  def save_for_backward(self, *x: Any) -> None:
+    """
+    Save tensors needed for gradient computation.
+    """
     self.saved_tensors.extend(x)
 
-  def apply(self, *x, **kwargs):
+  def apply(self, *x: Any, **kwargs: Any) -> Tensor:
     """
-    self  : is the tensor with data
-    *x    : the input to the method  
+    Apply the operation to input tensors.
+    
+    Sets up the computation graph for automatic differentiation.
     """
     op = self # self is the operation class
     ctx = op(*x)
@@ -187,20 +317,18 @@ class Function:
     ret._ctx = ctx
     return ret
 
-def register(name, fxn, gpu=False):
+def register(name: str, fxn: Any, gpu: bool = False) -> None:
   """
-  mechanism that allows you to chain methods in an intuitive and Pythonic way
-  e.g. x.dot(w).relu(), where w is a tensor
-
-  partialmethod is used to create a new method that has some of the arguments to
-  another method already filled in the apply method of that instance is added
+  Register an operation on the Tensor class to allow method chaining.
+  
+  Enables syntax like x.dot(w).relu() where w is a tensor.
   """
   if gpu: 
     Tensor.ops_gpu[name] = fxn
   else:
     Tensor.ops[name] = fxn
 
-  def dispatch(self, *x, **kwargs):
+  def dispatch(self: Tensor, *x: Any, **kwargs: Any) -> Tensor:
     op_func = (Tensor.ops_gpu if self.gpu else Tensor.ops)[name]
     op_func.cl_ctx, op_func.cl_queue = cl_ctx, cl_queue
     return op_func.apply(op_func, self, *x, **kwargs)
@@ -209,7 +337,7 @@ def register(name, fxn, gpu=False):
 
   if name in ['add', 'sub', 'mul', 'div']:
     setattr(Tensor, "__%s__" % name, dispatch)
-    setattr(Tensor, "__i%s__" % name, lambda self,x: self.assign(dispatch(self,x)))
+    setattr(Tensor, "__i%s__" % name, lambda self, x: self.assign(dispatch(self, x)))
 
 import froog.ops # this registers all the operations
 if GPU:
